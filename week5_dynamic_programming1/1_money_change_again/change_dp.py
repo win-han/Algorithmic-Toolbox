e8a920ecf2842f49a6290e673a7ff567eda3a3a3
gas21 <hans@berea.edu>
2018-09-23 17:49:29 -0400

    get change

--- week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -1,13 +1,15 @@
 # Uses python3
 import sys
+from operator import itemgetter
 
 
 def get_change(m):
-    sol = [[1], [1, 1], [3], [4]]
+    sol = [1, 2, 1, 1]
     for i in range(5, m + 1):
-        mini = sorted([sol[i-4], sol[i-3], sol[i-1]], key=len)
+        mini = sorted([(sol[-1],1), (sol[-3],3), (sol[-4],4)], key=itemgetter(0))
+        sol.append(mini[0]+1)
 
-    return m // 4
+    return sol[-1]
 
 
 if __name__ == '__main__':
